5ce918b6a671dab803ffcbf9ab6362af1a8d06de
Rohit <rohit.mmm1996@gmail.com>
2022-11-21 18:54:05 +0530

    Added vite build script

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,8 +10,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
-		outDir: 'build'
+		adapter: adapter()
 	}
 };
 
